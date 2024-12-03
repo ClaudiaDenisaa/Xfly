d888a4c87a98611b5368a9f5bfa6b76548b79366
ClaudiaDenisaa <denisaclaudia014@gmail.com>
2024-12-03 12:04:04 +0200

    -

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+//import java.swing.ImageIcon;
+import java.awt.Color;
 public class Main {
     public static void main(String[] args) {
+
+        MyFrame first_frame = new MyFrame();
 
     }
 }
