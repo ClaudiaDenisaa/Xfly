7b9db3b5c50ca565f1c1d272fbfa3510d77a0ed3
ClaudiaDenisaa <denisaclaudia014@gmail.com>
2024-12-03 12:05:00 +0200

    -

---
+++ src/main/java/MyFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+
+    //constructor
+    MyFrame() {
+        //JFrame frame = new JFrame();
+        this.setTitle("XFly");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(800, 600);
+        this.setVisible(true);
+
+
+    }
+}
